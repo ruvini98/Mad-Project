c4757d5f275af798b823e6d476d096e489494731
ChamaraSooriyapperuma <it19172842@my.sliit.lk>
2020-10-04 11:18:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/feedbackmanagement/Total.java
@@ -0,0 +1,35 @@
+package com.example.feedbackmanagement;
+
+public class Total {
+
+    private Integer student;
+    private Integer teacher;
+    private Integer total;
+
+    public Total() {
+    }
+
+    public Integer getStudent() {
+        return student;
+    }
+
+    public void setStudent(Integer student) {
+        this.student = student;
+    }
+
+    public Integer getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Integer teacher) {
+        this.teacher = teacher;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
